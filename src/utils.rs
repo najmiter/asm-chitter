c0445499fdc0dac5bed445f280170ea1321f604f
Najam <realnajmiter@gmail.com>
2024-12-15 18:21:31 +0500

    handle starting spaces

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::char;
+
 use crate::contants::asm_data;
 use crate::contants::Tokens;
 
@@ -6,41 +8,52 @@ use crate::contants::Tokens;
 // }
 
 pub fn parse_line(line: &str) -> Vec<Tokens> {
+    let data = asm_data();
     let mut array: Vec<Tokens> = Vec::new();
 
-    let data = asm_data();
-    let tokens = line.split(" ");
+    let [spaces, rest] = split_at_indent(line);
+
+    array.push(Tokens {
+        name: "space".to_string(),
+        class: "space".to_string(),
+        content: spaces.to_string(),
+    });
+
+    let tokens = rest.split(" ");
     let keys = data.keys();
     for token in tokens {
+        let mut map = Tokens {
+            name: "plain".to_string(),
+            class: "plain".to_string(),
+            content: token.to_string(),
+        };
         for key in keys.clone().into_iter() {
             let Some(asm_tokens) = data.get(key) else {
                 break;
             };
             if let Some(index) = asm_tokens.iter().position(|&x| x == token.to_uppercase()) {
-                let map = Tokens {
-                    name: key.to_string(),
-                    class: key.to_string(),
-                    content: token.to_string(),
-                };
-                array.push(map);
+                map.class = key.to_string();
+                map.name = key.to_string();
+                // let map = Tokens {
+                //     name: key.to_string(),
+                //     class: key.to_string(),
+                //     content: token.to_string(),
+                // };
+                // array.push(map);
                 println!("Found element at index: {}", index);
                 break;
             };
         }
-        println!("BOOM");
-
-        let map = Tokens {
-            name: "plain".to_string(),
-            class: "plain".to_string(),
-            content: token.to_string(),
-        };
+        // println!("BOOM");
         array.push(map);
     }
 
     array
 }
 
-/* let data = asm_data();
-if let Some(operators) = data.get("operators") {
-    return operators.join(", ");
-} */
+fn split_at_indent(string: &str) -> [&str; 2] {
+    [
+        &string[0..string.find(char::is_alphanumeric).unwrap_or(0)],
+        string.trim_start_matches(char::is_whitespace),
+    ]
+}
